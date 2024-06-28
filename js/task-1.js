d90438917f535f90006362bb6219f06acb3f22b2
AlicjaSzamraj <120415960+AlicjaSzamraj@users.noreply.github.com>
2024-06-28 20:27:58 +0100

    number of categories

---
+++ js/task-1.js
@@ -0,0 +1,4 @@
+const allCategories = document.querySelectorAll("ul#categories li.item");
+
+const numberOfCategories = allCategories.length;
+console.log(`Number of categories: ${numberOfCategories}`);
