8ef45b030caf3cb5e948705dd87aa82d29316e82
AlicjaSzamraj <120415960+AlicjaSzamraj@users.noreply.github.com>
2024-06-28 21:34:54 +0100

    second task done

--- js/task-1.js
+++ js/task-1.js
@@ -4,8 +4,8 @@ const numberOfCategories = allCategories.length;
 console.log(`Number of categories: ${numberOfCategories}`);
 
 allCategories.forEach((category) => {
-    const header = category.querySelector("h2").textContent;
-    const numberItems = category.querySelectorAll("li").length;
-    console.log(`Category: ${header}`);
-    console.log(`Elements: ${numberItems}`);
+  const header = category.querySelector("h2").textContent;
+  const numberItems = category.querySelectorAll("li").length;
+  console.log(`Category: ${header}`);
+  console.log(`Elements: ${numberItems}`);
 });
