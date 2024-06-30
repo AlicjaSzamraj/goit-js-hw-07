2a9d38f7a334b146beef23924177313eb76e6aae
AlicjaSzamraj <120415960+AlicjaSzamraj@users.noreply.github.com>
2024-06-30 17:42:51 +0100

    fourth task done

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,24 @@
+const registerForm = document.querySelector(".login-form");
 
+registerForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const loginForm = event.target;
+  const email = loginForm.elements.email;
+  const password = loginForm.elements.password;
+
+  if (!email.value.trim() || !password.value.trim()) {
+    alert("All form fields must be filled in");
+    return;
+  }
+
+  const formData = {
+    email: email.value.trim(),
+    password: password.value.trim(),
+  };
+
+  console.log("Form data:", formData);
+
+  registerForm.reset();
+}
