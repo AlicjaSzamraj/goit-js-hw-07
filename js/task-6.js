1a9c34d7731d9fd737bc29e806b4a79dc0081ddf
AlicjaSzamraj <120415960+AlicjaSzamraj@users.noreply.github.com>
2024-06-30 18:34:16 +0100

    sixth task done

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,35 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+function createBoxes(amount) {
+  const boxesContainer = document.querySelector("#boxes");
+  boxesContainer.innerHTML = "";
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.classList.add("box");
+    box.style.backgroundColor = getRandomHexColor();
+    box.style.width = `${30 + i * 10}px`;
+    box.style.height = `${30 + i * 10}px`;
+    boxesContainer.appendChild(box);
+  }
+}
+
+function destroyBoxes() {
+  const boxesContainer = document.querySelector("#boxes");
+  boxesContainer.innerHTML = "";
+}
+
+const createButton = document.querySelector("[data-create]");
+const destroyButton = document.querySelector("[data-destroy]");
+const input = document.querySelector("input");
+
+createButton.addEventListener("click", () => {
+  const amount = parseInt(input.value);
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    input.value = "";
+  }
+});
+
+destroyButton.addEventListener("click", destroyBoxes);
