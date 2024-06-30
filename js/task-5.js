815d16c98d25ef2293ce9ac4b8d88857061b7aae
AlicjaSzamraj <120415960+AlicjaSzamraj@users.noreply.github.com>
2024-06-30 18:12:08 +0100

    fifth task done

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,16 @@
+const body = document.body;
+const span = document.querySelector(".color");
+const button = document.querySelector(".change-color");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+button.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+
+  body.style.backgroundColor = randomColor;
+  span.style.textContent = randomColor;
+});
